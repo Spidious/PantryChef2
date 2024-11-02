02ed524ffea97b44232ffa1e3d450411113eac67
Luke <lukedeffenbaugh@gmail.com>
2024-11-01 22:24:42 -0500

    Fix accidental creation of Forms instead of WPF

---
+++ RecipeGen/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RecipeGen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
--- RecipeGen/obj/Debug/net8.0-windows/RecipeGen.AssemblyInfo.cs
+++ RecipeGen/obj/Debug/net8.0-windows/RecipeGen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeGen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80ffafbb0342fa4842cf2c8388a8c2223d9eedc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae70ea6d855197ecc4c3fa3f4800398e8de1b94")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeGen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeGen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
