9412833064a4212de0787a5c5ebf25953111cd4a
adr3wb <adr3wb@LFC1206-PC-49>
2024-11-02 15:04:19 -0500

    updated gitignore and insert ingredients query

--- RecipeGen/AddIngredientScreen.xaml.cs
+++ RecipeGen/AddIngredientScreen.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Data.SQLite;
 
 namespace RecipeGen
 {
@@ -26,6 +27,37 @@ namespace RecipeGen
 
         private void IngredientQuery()
         {
+            string connectionStr = "Data Source=C:\\Users\\adr3wb\\Source\\Repos\\RecipeGen2\\RecipeGen\\Data\\database.db;Version =3;";// C: \Users\adr3wb\Source\Repos\RecipeGen\RecipeGen\Data\database.db
+            using (SQLiteConnection connection = new SQLiteConnection(connectionStr))
+            {
+                connection.Open();
+                string input = IngredientNameTextBox.Text;
+                input = input.ToLower();
+                string[] ingredients = input.Split(',');
+                for (int i = 0; i < ingredients.Length; i++)
+                {
+                    string checkQuery = "SELECT COUNT(*) FROM ingredients WHERE name = @name;";
+                    using (SQLiteCommand checkCommand = new SQLiteCommand(checkQuery, connection))
+                    {
+                        input = ingredients[i].Trim();
+                        checkCommand.Parameters.AddWithValue("@name", input);
+
+                        long count = (long)checkCommand.ExecuteScalar(); // Execute the query and get the count
+
+                        if (count == 0) //if ingredient is not in table insert
+                        {
+                            string insertQuery = "INSERT INTO ingredients (name) VALUES (@name);";
+                            using (SQLiteCommand command = new SQLiteCommand(insertQuery, connection))
+                            {
+                                command.Parameters.AddWithValue("@name", input);
+                                command.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                }
+
+                connection.Close();
+            }
             CancelRequested?.Invoke();
         }
     }
