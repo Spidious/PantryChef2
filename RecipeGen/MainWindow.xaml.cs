259c4773084610108cae9dc9f6c443d8cbdf90f4
Luke <lukedeffenbaugh@gmail.com>
2024-11-02 09:48:59 -0500

    Add button functionality

--- RecipeGen/MainWindow.xaml.cs
+++ RecipeGen/MainWindow.xaml.cs
@@ -1,24 +1,43 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RecipeGen
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Event handler for the "Add Recipe" button in the "Recipes" tab
+        private void AddRecipeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a new TextBlock or custom control to represent a recipe item
+            TextBlock newRecipe = new TextBlock
+            {
+                Text = "New Recipe",
+                Margin = new Thickness(5),
+                Foreground = (System.Windows.Media.Brush)Resources["TextColor"]
+            };
+
+            // Add it to the RecipeWidgetsPlaceholder StackPanel
+            RecipeWidgetsPlaceholder.Children.Add(newRecipe);
+        }
+
+        // Event handler for the "Add Ingredient" button in the "Pantry" tab
+        private void AddIngredientButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a new TextBlock or custom control to represent an ingredient item
+            TextBlock newIngredient = new TextBlock
+            {
+                Text = "New Ingredient",
+                Margin = new Thickness(5),
+                Foreground = (System.Windows.Media.Brush)Resources["TextColor"]
+            };
+
+            // Add it to the IngredientListPlaceholder StackPanel
+            IngredientListPlaceholder.Children.Add(newIngredient);
         }
     }
 }
